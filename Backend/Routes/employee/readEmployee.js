d8b28e62d56b1dc7906e29734c8062d4ac1a6eda
Dhanushkabasnayaka99 <k9185.dhanuska@gmail.com>
2024-10-14 21:09:47 +0530

    change getImage

--- Backend/Routes/employee/readEmployee.js
+++ Backend/Routes/employee/readEmployee.js
@@ -6,35 +6,30 @@ import { getImage } from "../../AWS/get_images.js";
 const Employee = express.Router();
 
 Employee.get("/employeecard", async (req, res) => {
-    try {
-      const sql = "SELECT * FROM employer ";
-  
-      db.query(sql, async (err, ans) => {
-        if (err) return res.json({ Message: "Error inside server" });
-  
-        const employees = [];
-  
-      
-        const maxItems = Math.min(15, ans.length);
-        for (var i = 0; i < maxItems; i++) {
-       
+	try {
+		const sql = "SELECT * FROM employer ";
 
-          const imageUrl = await getImage(ans[i].image_link);
-  
-          
-          employees.push({
-            ...ans[i],
-            image_url: imageUrl
-          });
-        }
-      
-  
-        return res.json({ employees });
-      });
-    } catch (error) {
-      console.log(" error in get employee",error);
-      return res.json({ Message: "Error inside server" });
-    }
-  });
+		db.query(sql, async (err, ans) => {
+			if (err) return res.json({ Message: "Error inside server" });
 
-  export default Employee;
+			const employees = [];
+
+			const maxItems = Math.min(15, ans.length);
+			for (var i = 0; i < maxItems; i++) {
+				const imageUrl = await getImage(ans[i].image_link);
+
+				employees.push({
+					...ans[i],
+					image_url: imageUrl.url,
+				});
+			}
+
+			return res.json({ employees });
+		});
+	} catch (error) {
+		console.log(" error in get employee", error);
+		return res.json({ Message: "Error inside server" });
+	}
+});
+
+export default Employee;
